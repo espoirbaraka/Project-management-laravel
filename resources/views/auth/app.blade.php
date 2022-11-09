8f8b3efc218591d3f956b1ac82bef83b54f90998
espoirbaraka <esbarakabigeba@gmail.com>
2022-11-09 10:04:43 +0100

    Template

---
+++ resources/views/auth/app.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Gestion des projets</title>
+    <meta name="robots" content="noindex">
+    <link type="text/css" href="{{asset('assets/vendor/perfect-scrollbar.css')}}" rel="stylesheet">
+    <link type="text/css" href="{{asset('assets/css/app.css')}}" rel="stylesheet">
+    <link type="text/css" href="{{asset('assets/css/app.rtl.css')}}" rel="stylesheet">
+    <link type="text/css" href="{{asset('assets/css/vendor-material-icons.css')}}" rel="stylesheet">
+    <link type="text/css" href="{{asset('assets/css/vendor-material-icons.rtl.css')}}" rel="stylesheet">
+    <link type="text/css" href="{{asset('assets/css/vendor-fontawesome-free.css')}}" rel="stylesheet">
+    <link type="text/css" href="{{asset('assets/css/vendor-fontawesome-free.rtl.css')}}" rel="stylesheet">
+</head>
+
+<body class="layout-login">
+
+@yield('content')
+
+<script src="{{asset('assets/vendor/jquery.min.js')}}"></script>
+<script src="{{asset('assets/vendor/popper.min.js')}}"></script>
+<script src="{{asset('assets/vendor/bootstrap.min.js')}}"></script>
+<script src="{{asset('assets/vendor/perfect-scrollbar.min.js')}}"></script>
+<script src="{{asset('assets/vendor/dom-factory.js')}}"></script>
+<script src="{{asset('assets/vendor/material-design-kit.js')}}"></script>
+<script src="{{asset('assets/js/toggle-check-all.js')}}"></script>
+<script src="{{asset('assets/js/check-selected-row.js')}}"></script>
+<script src="{{asset('assets/js/dropdown.js')}}"></script>
+<script src="{{asset('assets/js/sidebar-mini.js')}}"></script>
+<script src="{{asset('assets/js/app.js')}}"></script>
+<script src="{{asset('assets/js/app-settings.js')}}"></script>
+</body>
+</html>

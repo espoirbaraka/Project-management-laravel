c071e4e557d25aa98935c2d5d4a84921ace8aa50
espoirbaraka <esbarakabigeba@gmail.com>
2022-11-14 22:19:41 +0100

    Other tables

---
+++ database/migrations/2022_11_14_211330_create_taches_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTachesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('taches', function (Blueprint $table) {
+            $table->id();
+            $table->text('designation');
+            $table->float('montant');
+            $table->dateTime('datedebut');
+            $table->dateTime('datefin');
+            $table->integer('codeprojet');
+            $table->integer('statut');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('taches');
+    }
+}

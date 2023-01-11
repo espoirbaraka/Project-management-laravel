66a73efe6dacf6cc90078b2d2313045f32a9ce58
espoirbaraka <esbarakabigeba@gmail.com>
2023-01-11 07:13:04 +0100

    image

--- resources/views/projet/newprojet.blade.php
+++ resources/views/projet/newprojet.blade.php
@@ -86,6 +86,15 @@
                                                 </div>
 
                                                 <div class="form-group">
+                                                    <label for="slug">Secteur d'action</label>
+                                                    <input id="slug"
+                                                           type="text"
+                                                           name="secteur"
+                                                           class="form-control"
+                                                           placeholder="">
+                                                </div>
+
+                                                <div class="form-group">
                                                     <label for="title">Date debut</label>
                                                     <input id="title"
                                                            type="date"
